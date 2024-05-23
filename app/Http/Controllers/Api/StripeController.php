197a6b0a6a1708fa1050d4671f861d56e39fab3c
Arnab Mukherjee <arnabm.ttl@gmail.com>
2024-05-23 12:58:43 +0530

    23/5/24 12:58

--- app/Http/Controllers/Api/StripeController.php
+++ app/Http/Controllers/Api/StripeController.php
@@ -10,9 +10,8 @@ class StripeController extends Controller
     //
     public function test(Request $request) {
 
-        // dd(getenv('STRIPE_PUBLISHABLE_KEY'));
-
-        $sk = getenv('STRIPE_SECRET_KEY');
+        // $sk = getenv('STRIPE_SECRET_KEY');
+        $sk = config('app.stripe_sk_test');
         $url ="https://api.stripe.com/v1/charges";
         $headers = array(
             'Accept: application/json',
@@ -33,17 +32,48 @@ class StripeController extends Controller
         // echo"<pre>";
         print_r($result);
         $res = json_decode($result);
-        print_r($res);
-        echo $res->url;
+        // print_r($res);
+        // echo $res->url;
         exit;
-
-        // return response()->json($result,  $headers);
-
-        
 
     }
 
     public function post(Request $request){
+        // $sk = getenv('STRIPE_SECRET_KEY');
+        $sk = config('app.stripe_sk_test');
+        $url ="https://api.stripe.com/v1/tokens";
+        $headers = array(
+            'Accept: application/json',
+            'Content-Type: application/x-www-form-urlencoded',
+            'Authorization: Bearer '.$sk
+        );
         
+        $ch = curl_init(); //open connection
+        curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+        curl_setopt($ch, CURLOPT_URL, $url);
+        // curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+
+        $postFields = array(
+            'card' => [
+                'number' => '4242424242424242',
+                'exp_month' => 12,
+                'exp_year' => 2024,
+                'cvc' => '314',
+            ],
+            'customer' => 'cus_Q7a6ZPVwIi1u00'
+        );
+
+        curl_setopt($ch, CURLOPT_POSTFIELDS,
+        json_encode ($postFields));
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        
+        curl_setopt($ch, CURLOPT_POST, 1);
+        // curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+        // curl_setopt($ch, CURLOPT_TIMEOUT, 180);
+        $result = curl_exec($ch); //execute post
+        // echo"<pre>";
+        print_r($result);
+        
+        exit;
     }
 }
