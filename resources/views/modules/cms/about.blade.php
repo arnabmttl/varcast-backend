faec52176e0d483fad2f2b0a0241f3c14449fbc4
Arnab Mukherjee <arnabm.ttl@gmail.com>
2024-05-29 19:55:43 +0530

    aboutus static pic upload

--- resources/views/modules/cms/about.blade.php
+++ resources/views/modules/cms/about.blade.php
@@ -14,12 +14,13 @@
     <div class="container">
         <div class="row justify-content-center align-items-center">
             <div class="col-lg-5 mb-lg-0 mb-4 pr-lg-5 {{--  wow  --}} zoomIn" data-wow-duration="1.5s" style="visibility: visible; animation-duration: 1.5s; animation-name: zoomIn;">
-                {{--  <img class="w-100" src="images/your-business-growth.jpg" alt="">  --}}
-                @if(@$about->image)
+                 <img class="w-100" src="{{ url('uploads/about-us/varcast-about-us.jpg') }}" alt=""> 
+                 {{-- <img class="w-100" src="images/your-business-growth.jpg" alt=""> frontend/images/logo.png  --}}
+                {{-- @if(@$about->image)
                     <img src="{{url('storage/content/'.@$about->image)}}" class="w-100" alt="{{ @$about->name }}">
                 @else
                     <img src="{{url('images/no-image.png')}}" class="w-100" style="width: 88px;">
-                @endif
+                @endif --}}
             </div>
             <div class="col-lg-5 {{--  wow fadeInRight  --}}" data-wow-duration="2s" style="visibility: visible; animation-duration: 2s; animation-name: fadeInRight;">
                 <h2>{{ @$about->name }}</h2>
