8c45e8c58afc93b9acc6ca0c6ee10197df4d4f87
Arnab Mukherjee <arnabm.ttl@gmail.com>
2024-06-07 15:48:42 +0530

    7/6/24 15:48

--- app/Models/VideoDraft.php
+++ app/Models/VideoDraft.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Jenssegers\Mongodb\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
-use App\Models\VideoCategory;
+use App\Models\VideoDraftCategory;
 
 
 class VideoDraft extends Model
@@ -22,6 +22,6 @@ class VideoDraft extends Model
      */
     public function categories(): HasMany
     {
-        return $this->hasMany(VideoCategory::class, 'videoId', '_id');
+        return $this->hasMany(VideoDraftCategory::class, 'videoDraftId', '_id');
     }
 }
